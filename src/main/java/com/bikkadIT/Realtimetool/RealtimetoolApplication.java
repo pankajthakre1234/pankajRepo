6044149044e57bead3f2c30cd0e221e652736398
pankajthakre1234 <pankajthakre786@gmail.com>
2023-01-29 17:53:21 +0530

    chnages by Pankaj

---
+++ src/main/java/com/bikkadIT/Realtimetool/RealtimetoolApplication.java
@@ -0,0 +1,13 @@
+package com.bikkadIT.Realtimetool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimetoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealtimetoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bikkadIT/Realtimetool/RealtimetoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bikkadIT.Realtimetool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimetoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
